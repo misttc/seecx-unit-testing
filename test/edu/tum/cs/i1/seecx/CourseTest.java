e8dafe8ee22c59293f73b98d49a3f9394b0eac6f
misttc <misttc@gmail.com>
2018-05-16 13:52:49 +0800

    initial update
    add three tests

--- test/edu/tum/cs/i1/seecx/CourseTest.java
+++ test/edu/tum/cs/i1/seecx/CourseTest.java
@@ -10,8 +10,40 @@ import java.util.List;
 
 public class CourseTest {
 
+	@Test
+	public void testGetTitle() {
+		String CourseName = "Software Engineering";
+		Course aCourse = new Course(CourseName);
+		
+		assertEquals("Get Course Name Match",aCourse.getTitle(),CourseName);
+	}
+
+	@Test
+	public void testGetNumberOfAttendeesWithNoAttendees() {
+
+		String CourseName = "Data Mining";
+		Course aCourse = new Course(CourseName);
+		
+		assertSame("No one enrolled yet",aCourse.getNumberOfAttendees(),0);		
+	}
+
+	@Test
+	public void testGetNumberOfAttendeesWithTwoAttendees() {
+
+		String CourseName = "Data Mining";
+		
+		Course aCourse = new Course(CourseName);
+		List<Student> attendees = new ArrayList<Student>();
+		
+		Student s1, s2;
+		
+		s1 = new Student("Don","Trump","1950","MIS","MKT");
+		s2 = new Student("Bill","Clinton","1954","LAW","MKT");
 	
-
-
+		attendees.add(s1);
+		attendees.add(s2);
+		aCourse.setAttendees(attendees);
+		assertSame("Two enrolled",aCourse.getNumberOfAttendees(),2);				
+	}
 
 }
